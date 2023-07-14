14a3465c3ac46a475f9e810712d505a650e90e34
unknown <zaidj777@gmail.com>
2023-07-14 23:22:48 +0530

    Header Completed

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { AppBar, Toolbar, Typography, styled } from '@mui/material';
+import { logo } from './constants/constants';
+
+
+const Header : React.FC = () => {
+  return (
+    <div>
+        <AppBar color="transparent">
+            <Toolbar >
+                <img src={logo} alt="logo"style={{ width: '30px', marginRight: '10px'}} />
+                <Typography>EverNote</Typography>
+            </Toolbar>
+        </AppBar>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/components/constants/constants.js
@@ -0,0 +1,2 @@
+
+export const logo = 'https://www.freepnglogos.com/uploads/walmart-logo-7.jpeg'
